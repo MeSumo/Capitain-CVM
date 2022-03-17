1a450ad02c9300df0d60b489dc1f574555e3ea57
Abd Alla Sumer <1962935@decinfo.cvm>
2022-03-17 12:43:32 -0400

    remplie fichier collectablesui

--- Assets/Scripts/Collectable et UI/CollectablesUI.cs
+++ Assets/Scripts/Collectable et UI/CollectablesUI.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectablesUI : MonoBehaviour
 {
+    /// <summary>
+    /// Liste de GO représentant le nombre de chaque collectable collecté
+    /// </summary>
+    [SerializeField]
+    private GameObject[] _textes;
+    /// <summary>
+    /// Liste du nombre de collectable collecté selon le playerData
+    /// // index 0 -> convention, 1 -> cartemembre, 2 -> paycheck
+    /// </summary>
+    private int[] _nbCollectablesList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _nbCollectablesList = GameManager.Instance.PlayerData.ListeNombreCollectables;
+        for(int i = 0; i < _textes.Length; i++)
+        {
+            _textes[i].GetComponent<TextMeshProUGUI>().text = _nbCollectablesList[i].ToString();
+        }
     }
 
     // Update is called once per frame
